7c2298f037cdbb695dc805da044c787ac18923fe
lweislak <150990328+lweislak@users.noreply.github.com>
2025-01-28 17:35:26 -0800

    Refactored functions

--- experiment3/js/sketch.js
+++ experiment3/js/sketch.js
@@ -21,13 +21,18 @@ class Particle {
       this.velocity.setMag(2, 4); //Moves particles at different speeds
       this.acceleration = createVector(0,0);
       this.maxForce = 0.2;
-      this.maxSpeed = 4;
+      this.maxSpeed = 7;
     }
 
-    show() {
+    show(color) {
       strokeWeight(8);
-      stroke(255);
+      //stroke(255);
+      stroke(color);
       point(this.position.x, this.position.y);
+    }
+
+    checkColor() {
+
     }
 
     update() {
@@ -44,6 +49,10 @@ class Particle {
       this.acceleration.add(separation);
       this.acceleration.add(alignment); //Force accumulation
       this.acceleration.add(cohesion);
+
+      //console.log(Math.abs(Math.round((this.velocity.x + this.velocity.y)*100)));
+
+      return(Math.abs(Math.round((this.velocity.x + this.velocity.y)*100)));
     }
 
     //Wrap particles around screen
@@ -61,6 +70,34 @@ class Particle {
       }
     }
 
+    getSteeringForce(particles, rule) {
+      let radius = 20;
+      let total = 0;
+      let steeringForce = createVector();
+      for (let other of particles) {
+        let d = dist(this.position.x, this.position.y, other.position.x, other.position.y);
+        if(other != this && d < radius) {
+          if(rule == "align") {steeringForce.add(other.velocity);}
+          else if(rule == "cohesion") {steeringForce.add(other.position);}
+          else { //Separation
+            let difference = p5.Vector.sub(this.position, other.position);
+            difference.div(d**2); //The farther away a particle is, the lower the magnitude
+            steeringForce.add(difference);
+          }
+          total++;
+        }
+      }
+      if (total > 0) {
+        steeringForce.div(total);
+        if(rule == "cohesion") { steeringForce.sub(this.position); }
+        steeringForce.setMag(this.maxSpeed);
+        steeringForce.sub(this.velocity);
+        steeringForce.limit(this.maxForce);
+      }
+      return steeringForce;
+    }
+
+    /*
     //TODO: Refactor!
     //Align particle with other local particles
     align(particles) {
@@ -128,6 +165,19 @@ class Particle {
       }
       return steeringForce;
     }
+    */
+
+    align(particles) {
+      return(this.getSteeringForce(particles, "align"));
+    }
+
+    cohesion(particles) {
+      return(this.getSteeringForce(particles, "cohesion"));
+    }
+
+    separation(particles) {
+      return(this.getSteeringForce(particles, "separation"));
+    }
 }
 
 function resizeScreen() {
@@ -156,12 +206,13 @@ function setup() {
 
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
-  background(51);
+  //background(51);
+  background("#0080ff");
 
   for(let particle of flock) {
     particle.edges();
-    particle.flock(flock);
+    testColor = particle.flock(flock);
     particle.update();
-    particle.show();
+    particle.show(testColor);
   }
 }
