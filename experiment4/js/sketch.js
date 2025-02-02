c91a2ea27ce963589ef2971ba3428cb2e2ac20d0
lweislak <150990328+lweislak@users.noreply.github.com>
2025-02-02 01:19:44 -0800

    Play sound when chime is clicked

--- experiment4/js/sketch.js
+++ experiment4/js/sketch.js
@@ -16,6 +16,7 @@ class Chime {
 
   display() {
     strokeWeight(1);
+    stroke("black");
     let radius = this.width/2;
 
     //Draw chime
@@ -29,9 +30,15 @@ class Chime {
     line(this.position.x, this.position.y, this.position.x, 0); //Draw string
   }
 
-  //Check if chime has been pressed
-  checkClick() {
-
+  //Check if chime has been clicked
+  wasClicked() {
+    if(mouseX < this.position.x + this.width/2 && mouseX > this.position.x - this.width/2) {
+      if(mouseY < this.position.y + this.height && mouseY > this.position.y) {
+        console.log("CLICKED");
+        return true;
+      }
+    }
+    return false;
   }
 }
 
@@ -55,17 +62,30 @@ function setup() {
   });
   resizeScreen();
 
+  chimes = [];
+
   testChime = new Chime(600, 200); //TEST
   testChime2 = new Chime(900, 350);
+  chimes.push(testChime);
+  chimes.push(testChime2);
 }
 
 function draw() {
   background(220);
 
-  testChime.display(); //TEST
-  testChime2.display();
+  for(let chime of chimes) {
+    chime.display();
+
+    strokeWeight(8); //TEST
+    stroke("red");
+    point(chime.position.x, chime.position.y);
+  }
 }
 
 function mousePressed() {
-  testChime.sound.play(); //TEST
+  for(let chime of chimes) {
+    if(chime.wasClicked()) {
+      chime.sound.play();
+    }
+  }
 }
