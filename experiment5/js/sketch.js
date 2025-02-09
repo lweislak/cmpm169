83d6c2d336a75aac32bf1f5850df52fb4d5e156c
lweislak <150990328+lweislak@users.noreply.github.com>
2025-02-09 00:11:32 -0800

    Created experiment 5

---
+++ experiment5/js/sketch.js
@@ -0,0 +1,32 @@
+// sketch.js - purpose and description here
+// Author: Lo Weislak
+// Date: 2/10/25
+
+// Globals
+let canvasContainer;
+var centerHorz, centerVert;
+
+function resizeScreen() {
+  centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
+  centerVert = canvasContainer.height() / 2; // Adjusted for drawing logic
+  console.log("Resizing...");
+  resizeCanvas(canvasContainer.width(), canvasContainer.height());
+  // redrawCanvas(); // Redraw everything based on new size
+}
+
+function setup() {
+  // place our canvas, making it fit our container
+  canvasContainer = $("#canvas-container");
+  let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
+  canvas.parent("canvas-container");
+  // resize canvas is the page is resized
+
+  $(window).resize(function() {
+    resizeScreen();
+  });
+  resizeScreen();
+}
+
+function draw() {
+  background(0);
+}
