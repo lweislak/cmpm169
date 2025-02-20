d1a82abeb4840aab6ece8cb98b339726111b3014
lweislak <150990328+lweislak@users.noreply.github.com>
2025-02-20 00:55:05 -0800

    Added word rotation and mirror font

--- experiment6/js/sketch.js
+++ experiment6/js/sketch.js
@@ -1,16 +1,40 @@
-// sketch.js -
+// sketch.js - Click to display random word from text file.
+// Font creates a mirror effect so words are readable when rotated on the x-axis
 
 // Author: Lo Weislak
 // Date: 2/17/25
 
 //Random words text file: https://github.com/Shreda/pentestTools/blob/master/random-words.txt
-//Text inspo: https://editor.p5js.org/Jennybkowalski/sketches/N-geQ3DRx
+//Lake Reflection font: https://fontmeme.com/fonts/lake-reflection-font/
 
 // Globals
 let canvasContainer;
 var centerHorz, centerVert;
 
 let words = [];
+let totalWords = [];
+
+class Word {
+  constructor(word, x, y) {
+    this.word = word;
+    this.position = {x: x - width/2, y: y - height/2};
+    this.angle = 0;
+    this.blue = random() * 255;
+    this.green = random() * 255;
+  }
+
+  draw() {
+    push();
+    translate(this.position.x, this.position.y, 0);
+    rotateX(this.angle);
+    fill(0, this.green, this.blue);
+    //text(this.word, this.position.x, this.position.y);
+    textAlign(CENTER);
+    text(this.word, 0, 0);
+    this.angle += 0.01;
+    pop();
+  }
+}
 
 function resizeScreen() { 
   centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
@@ -21,8 +45,8 @@ function resizeScreen() {
 }
 
 function preload() {
-  words = loadStrings('./random-words.txt'); //Load file into array
-  font = loadFont('Consolas.ttf');
+  totalWords = loadStrings('./random-words.txt'); //Load file into array
+  font = loadFont('./LakeReflection.otf');
 }
 
 function setup() {
@@ -37,21 +61,32 @@ function setup() {
   });
   resizeScreen();
 
-  background(255);
+  background(10);
 }
 
 function draw() {
+  background(10);
+
   textFont(font);
   textAlign(CENTER,CENTER);
   textSize(50);
-  //rotateX(map(mouseX,0,width,0,TWO_PI));
-  //rotateY(map(mouseY,0,height,0,TWO_PI));
-  //rotateZ(map(frameCount,0,600,0,TWO_PI))
-  push();
-  for(let i = 0; i < 100; i++) {
-    fill(map(i, 0, 100, 0, 255), 80, 215);
-    translate(0, 0, 0.5); //Give text depth
-    text("Hello", 0, 0);
+
+  for(let word of words) {
+    word.draw();
   }
-  pop();
+}
+
+function mouseClicked() {
+  if(canvasPressed()) {
+    //words.push(new Word(totalWords.pop(), mouseX, mouseY));
+    words.push(new Word(totalWords.splice(random()*totalWords.length, 1), mouseX, mouseY)); //Select and remove a random word from array
+  }
+}
+
+//Helper function to determine if mouse was pressed on the canvas
+function canvasPressed() {
+	if((mouseX > 0 && mouseX < width) && (mouseY > 0 && mouseY < height)) {
+		return true;
+	}
+	return false;
 }
