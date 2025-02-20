8ea6bbe61482a0b165ee6a0bbb2617df1347159d
lweislak <150990328+lweislak@users.noreply.github.com>
2025-02-19 18:48:22 -0800

    Basic 3D text

--- experiment6/js/sketch.js
+++ experiment6/js/sketch.js
@@ -3,9 +3,13 @@
 // Author: Lo Weislak
 // Date: 2/17/25
 
+//Random words text file: https://github.com/Shreda/pentestTools/blob/master/random-words.txt
+
 // Globals
 let canvasContainer;
 var centerHorz, centerVert;
+
+let words = [];
 
 function resizeScreen() { 
   centerHorz = canvasContainer.width() / 2; // Adjusted for drawing logic
@@ -13,6 +17,11 @@ function resizeScreen() {
   console.log("Resizing...");
   resizeCanvas(canvasContainer.width(), canvasContainer.height());
   // redrawCanvas(); // Redraw everything based on new size
+}
+
+function preload() {
+  words = loadStrings('./random-words.txt'); //Load file into array
+  font = loadFont('Consolas.ttf');
 }
 
 function setup() {
@@ -31,5 +40,17 @@ function setup() {
 }
 
 function draw() {
-
+  textFont(font);
+  textAlign(CENTER,CENTER);
+  textSize(50);
+  //rotateX(map(mouseX,0,width,0,TWO_PI));
+  //rotateY(map(mouseY,0,height,0,TWO_PI));
+  //rotateZ(map(frameCount,0,600,0,TWO_PI))
+  push();
+  for(let i=0; i<100; i++) {
+    fill(map(i,0,100,0,255),80,215);
+    translate(0,0,0.5); //Give text depth
+    text("Hello",0,0);
+  }
+  pop();
 }
