d2bd760a5ac9fa8e0a92fd7ede4cf3516a63a20b
lweislak <150990328+lweislak@users.noreply.github.com>
2025-02-24 18:16:49 -0800

    Basic scrolling along line

--- experiment7/js/sketch.js
+++ experiment7/js/sketch.js
@@ -27,21 +27,67 @@ function setup() {
   });
   resizeScreen();
 
-  background(0);
+  background("rgb( 0, 0, 30)");
 }
 
-let circleSize = 100;
+let scrollAmount = 50;
+let blue = 30;
+let green = 0;
+
+let lines = [];
 
 function draw() {
+  //background(`rgb(0, ${green} , ${blue})`);
   background(15);
-	circle(0, 0, circleSize);
+
+  push();
+  translate(-scrollAmount, 0);
+  strokeWeight(5);
+  stroke("green");
+	line(0, 0, scrollAmount, 0);
+
+  drawLines();
+  pop();
 }
 
 function mouseWheel(event) {
+  calculateLines();
+
+  //TODO: Fix!!
+   //Stop user from scrolling to the left
+  if(scrollAmount < 50) {
+    scrollAmount = 50;
+    //background(`rgb(0, 0, 30)`);
+    return false;
+  }
+
+  //Determine if scrolling up or down
 	if(event.delta > 0){
-		circleSize += 10;
+		scrollAmount += 10;
+    green++;
 	} else {
-		circleSize -= 10;
+		scrollAmount -= 10;
+    green--;
 	}
-	return false; //Prohibit scroll
+	return false; //Prohibits page scroll
+}
+
+function calculateLines() {
+  //Add new lines
+  if(scrollAmount % 200 == 0) {
+    lines.push(scrollAmount);
+  }
+
+  //Remove lines when scrolling back
+  for(let currLine of lines) {
+    if(currLine > scrollAmount) {
+      lines.splice(lines.indexOf(currLine, 1));
+    }
+  }
+}
+
+function drawLines() {
+  for(var currLine of lines) {
+    line(currLine, -10, currLine, 10);
+  }
 }
